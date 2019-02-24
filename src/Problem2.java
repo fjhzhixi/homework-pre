0cd1d5c6c2af9ca083871997201d589d11ccc57d
fjh <18810827418@163.com>
2019-02-24 12:01:24 +0800

    change Problem2.java

--- src/Problem2.java
+++ src/Problem2.java
@@ -2,29 +2,6 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Problem2 {
-    /*public static void main(String[] args) {
-        BigInteger a, b;
-        a = new BigInteger("0");
-        b = new BigInteger("0");
-        Scanner input = new Scanner(System.in);
-        if (input.hasNextBigInteger()) {
-            a = input.nextBigInteger();
-        } else {
-            System.out.println("WRONG FORMAT!");
-            System.exit(0);
-        }
-        if (input.hasNextBigInteger()) {
-            b = input.nextBigInteger();
-        } else {
-            System.out.println("WRONG FORMAT!");
-            System.exit(0);
-        }
-        if (input.hasNext()) {
-            System.out.println("WRONG FORMAT!");
-            System.exit(0);
-        }
-        System.out.println(a.add(b));
-    }*/
 
     public static void main(String[] args){
         String str;
